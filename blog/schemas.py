a1d21181ccac7c578cfa448e00995f6a1dda5a11
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-16 03:17:53 +0500

    feat: added 3 new classes, understood how ORM works with pydantic and sqlalchemy

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,6 +1,23 @@
 from pydantic import BaseModel
 
+#in pydantic, we call it not model but schemas
 
 class Blog(BaseModel):
     title: str
     body: str
+
+class Showblog(BaseModel):
+    body: str
+    class Config():
+        from_attributes = True
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    password: str
+    class Config():
+        from_attributes = True
