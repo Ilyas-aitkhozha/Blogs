4831fa210c3b431ecc879364a488ce26dd5f75d9
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-18 00:02:24 +0500

    feat: added current_user into the all of the functions

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -14,20 +14,20 @@ router = APIRouter(
 
 get_db = database.get_db
 @router.get("/", response_model=List[schemas.Showblog])
-def get_blogs(db:Session = Depends(get_db), get_current_user: schemas.User = Depends(oaut2.get_current_user)):
+def get_blogs(db:Session = Depends(get_db), current_user: schemas.User = Depends(oaut2.get_current_user)):
     return get_all(db)
 @router.post("/", status_code = status.HTTP_201_CREATED)
-def create(request:schemas.Blog, db:Session = Depends(get_db)):
+def create(request:schemas.Blog, db:Session = Depends(get_db),current_user: schemas.User = Depends(oaut2.get_current_user)):
     return blog.create(db, request)
 
 @router.delete("/{id}", status_code = status.HTTP_204_NO_CONTENT)
-def delete_blog_by_id(id: int, db:Session = Depends(get_db)):
+def delete_blog_by_id(id: int, db:Session = Depends(get_db), current_user: schemas.User = Depends(oaut2.get_current_user)):
     return blog.delete(id, db)
 
 @router.put('/{id}', status_code = status.HTTP_202_ACCEPTED)
-def update_blog_by_id(id:int, request:schemas.Blog, db:Session = Depends(get_db)):
+def update_blog_by_id(id:int, request:schemas.Blog, db:Session = Depends(get_db), current_user: schemas.User = Depends(oaut2.get_current_user)):
     return blog.update(id, request, db)
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model = schemas.Showblog)
-def get_blog_by_id(id:int, db:Session = Depends(get_db)):
+def get_blog_by_id(id:int, db:Session = Depends(get_db), current_user: schemas.User = Depends(oaut2.get_current_user)):
     return blog.show(id, db)
