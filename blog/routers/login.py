40edfc285b5df2521249414fb0cac45360d59c90
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-19 10:07:29 +0500

    added sub user.id, now checkign for user_id

--- blog/routers/login.py
+++ blog/routers/login.py
@@ -10,9 +10,7 @@ router = APIRouter(
 @router.post("/login")
 def login(request:OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    if not Hash.verify(user.password, request.password):
-        raise HTTPException(status_code=404, detail="password incorrect")
-    access_token = jwttoken.create_access_token(data={"sub": user.email})
+    if not user or not Hash.verify(user.password, request.password):
+        raise HTTPException(status_code=404, detail="Invalid credentials")
+    access_token = jwttoken.create_access_token(data={"sub": str(user.id)})
     return {"access_token": access_token, "token_type": "bearer"}
