43178b398adda750c20d824e76535ad71d357b9c
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-19 10:07:02 +0500

    feat: so i was hardcoding user_id = 1, so now, im gettin current user, and giving it to my routers

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -3,7 +3,6 @@ from .. import schemas, database, oaut2
 from typing import List
 from sqlalchemy.orm import Session
 from ..repository import blog
-from ..repository.blog import get_all
 
 router = APIRouter(
     prefix="/blog",
@@ -13,21 +12,32 @@ router = APIRouter(
 
 
 get_db = database.get_db
+
+
 @router.get("/", response_model=List[schemas.Showblog])
-def get_blogs(db:Session = Depends(get_db), current_user: schemas.User = Depends(oaut2.get_current_user)):
-    return get_all(db)
+def get_all_blogs(db: Session = Depends(get_db)):
+    return blog.get_all(db)
+
+@router.get("/yours", response_model=List[schemas.Showblog])
+def get_your_blogs(
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(oaut2.get_current_user)
+):
+    return blog.get_all_yours(db, current_user)
+
+
 @router.post("/", status_code = status.HTTP_201_CREATED)
 def create(request:schemas.Blog, db:Session = Depends(get_db),current_user: schemas.User = Depends(oaut2.get_current_user)):
-    return blog.create(db, request)
+    return blog.create(db, request, current_user)
 
 @router.delete("/{id}", status_code = status.HTTP_204_NO_CONTENT)
 def delete_blog_by_id(id: int, db:Session = Depends(get_db), current_user: schemas.User = Depends(oaut2.get_current_user)):
-    return blog.delete(id, db)
+    return blog.delete(id, db, current_user)
 
 @router.put('/{id}', status_code = status.HTTP_202_ACCEPTED)
 def update_blog_by_id(id:int, request:schemas.Blog, db:Session = Depends(get_db), current_user: schemas.User = Depends(oaut2.get_current_user)):
-    return blog.update(id, request, db)
+    return blog.update(id, request, db, current_user)
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model = schemas.Showblog)
 def get_blog_by_id(id:int, db:Session = Depends(get_db), current_user: schemas.User = Depends(oaut2.get_current_user)):
-    return blog.show(id, db)
+    return blog.show(id, db, current_user)
