fa6453f3d23c8b8db23f5414cde35bccab0abcfa
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-19 10:06:07 +0500

    feat: current user updating transformed, now returning user_id, with models.User

--- blog/oaut2.py
+++ blog/oaut2.py
@@ -1,15 +1,23 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from . import jwttoken
+from . import models, database
+from sqlalchemy.orm import Session
+
 
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
-def get_current_user(tokendata: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)) -> models.User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    return jwttoken.verify_token(tokendata, credentials_exception)
+    user_id = jwttoken.verify_token(token, credentials_exception)
+
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise credentials_exception
+    return user
