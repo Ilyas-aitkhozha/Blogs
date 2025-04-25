b84ad1f44514577423f32eced4e806a13ccb58f6
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-26 01:54:34 +0500

    on the way to add gemini to answer

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import blog, user, login
+from .routers import blog, user, login, chat_bot
 
 
 app = FastAPI(
@@ -13,6 +13,7 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(login.router)
 app.include_router(blog.router)
 app.include_router(user.router)
+app.include_router(chat_bot.router)
 
 
 
