af02e37eaf9a135b26ab4d7e4cc0150fb8c6e387
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-26 18:22:27 +0500

    rn migrating in postgresql db, rather than in sqlite, for the render cloud

--- blog/main.py
+++ blog/main.py
@@ -2,14 +2,16 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import blog, user, login, chat_bot
+from contextlib import asynccontextmanager
 
 
-app = FastAPI(
 
-)
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    models.Base.metadata.create_all(bind=engine)
+    yield
 
-models.Base.metadata.create_all(bind=engine)
-
+app = FastAPI(lifespan=lifespan)
 app.include_router(login.router)
 app.include_router(blog.router)
 app.include_router(user.router)
