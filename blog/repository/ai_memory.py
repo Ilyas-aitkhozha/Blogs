246ee4f000c37cf7b46436ea88ea0295bd22edbd
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-27 10:56:08 +0500

    feat: everytime user join website he automatically creates a session

--- blog/repository/ai_memory.py
+++ blog/repository/ai_memory.py
@@ -1,5 +1,17 @@
 from sqlalchemy.orm import Session
 from ..models import ChatMessage
+from blog.models import SessionRecord
+
+def create_session(db: Session, session_id: str):
+    new_session = SessionRecord(id=session_id)
+    db.add(new_session)
+    db.commit()
+    db.refresh(new_session)
+    return new_session
+
+def get_session(db: Session, session_id: str):
+    return db.query(SessionRecord).filter(SessionRecord.id == session_id).first()
+
 
 def get_history(db:Session, session_id: str):
     return(
