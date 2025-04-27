96b35decd92cd4197e2fdaf626ca41cadf550651
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-27 11:03:03 +0500

    forgot to import modules))

--- blog/routers/chat_bot.py
+++ blog/routers/chat_bot.py
@@ -3,7 +3,7 @@ from fastapi import Depends, APIRouter, Header, HTTPException
 from sqlalchemy.orm import Session
 from ..database import get_db
 from ..schemas import ChatRequest, ChatResponse
-from ..repository.ai_service import generate_reply
+from ..repository import ai_service, ai_memory
 
 router = APIRouter(
     prefix="/chat",
@@ -18,9 +18,9 @@ def chat(
 ):
     if session_id is None:
         session_id = str(uuid.uuid4())
-        create_session(db, session_id)
+        ai_memory.create_session(db, session_id)
     else:
-        if not get_session(db, session_id):
+        if not ai_memory.get_session(db, session_id):
             raise HTTPException(status_code=400, detail="Session ID not found")
-    reply = generate_reply(db, session_id, req.message)
+    reply = ai_service.generate_reply(db, session_id, req.message)
     return ChatResponse(reply=reply)
