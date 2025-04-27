92a83eb436d33b1c886ae4cac93b744bcc409df3
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-27 11:13:00 +0500

    chat_history

--- blog/repository/ai_service.py
+++ blog/repository/ai_service.py
@@ -9,22 +9,17 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def generate_reply(db, session_id, user_input):
     save_message(db, session_id, role="user", content=user_input)
-
     history = list(reversed(get_history(db, session_id)))
-
-    messages = [{"role": "user", "parts": ["You are a helpful assistant."]}]
+    messages = [{"role": "user", "parts": ["You are a helpful in the website about blogs assistant."]}]
     for msg in history:
         messages.append({
             "role": msg.role if msg.role in ["user", "model", "assistant"] else "user",
             "parts": [msg.content]
         })
-    messages.append({"role": "user", "parts": [user_input]})
 
     model = GenerativeModel("gemini-1.5-flash")
-
     response = model.generate_content(messages)
     reply = response.text.strip()
-
     save_message(db, session_id, role="assistant", content=reply)
-
     return reply
+
