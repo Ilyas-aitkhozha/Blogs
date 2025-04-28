66e9f1feb3b69e58568d9624bc0e2cb08660a551
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-28 21:08:57 +0500

    all crud operations here

--- blog/repository/user.py
+++ blog/repository/user.py
@@ -1,31 +1,29 @@
-from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from .. import schemas, models
-from ..hashing import Hash
+from blog import models
+from blog.hashing import Hash
+from blog.schemas.user import UserCreate
+from pydantic import EmailStr
+from fastapi import HTTPException
 
-def create(request:schemas.User, db:Session):
-    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
+def create_user(db: Session, user: UserCreate):
+    new_user = models.User(
+        name=user.name,
+        email=str(user.email),
+        password=Hash.bcrypt(user.password)
+    )
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
 
-def show(id:int, db:Session):
-    user = db.query(models.User).filter(models.User.id == id).first()
+def get_user_by_id(db: Session, user_id: int):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
     if not user:
-        raise HTTPException(status_code=404, detail=f"user with id {id} not found")
+        raise HTTPException(status_code=404, detail=f"User with id {user_id} not found")
     return user
 
-def delete_by_id(id:int, db:Session):
-    users = db.query(models.User).filter(models.User.id == id)
-    if not users:
-        raise HTTPException(status_code=404, detail=f"user with id {id} not found")
-    else:
-        users.delete(synchronize_session=False)
-        db.commit()
-        return f"user with this id {id} was deleted"
+def get_user_by_email(db: Session, email: EmailStr):
+    return db.query(models.User).filter(models.User.email == email).first()
 
-def show_all(db):
-    users = db.query(models.User).all()
-    return users
-
+def get_all_users(db: Session):
+    return db.query(models.User).all()
