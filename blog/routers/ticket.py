14ef1c9c0caeb65f565eb569f0afd149383335bf
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-28 21:07:46 +0500

    deleted blog router, right now ticket is the main thing

--- blog/routers/ticket.py
+++ blog/routers/ticket.py
@@ -1,43 +1,36 @@
-from fastapi import APIRouter, Depends, status
-from .. import schemas, database, oaut2
-from typing import List
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from ..repository import ticket
+from blog.schemas import ticket as ticket_schema
+from blog.database import get_db
+from blog.repository import ticket as ticket_repository
+from blog.oaut2 import get_current_user
+from blog import models
 
 router = APIRouter(
-    prefix="/blog",
-    tags = ['blogs']
-
+    prefix="/tickets",
+    tags=["Tickets"]
 )
 
+@router.post("/", response_model=ticket_schema.TicketOut)
+def create_ticket(request: ticket_schema.TicketCreate, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+    return ticket_repository.create_ticket(db, request, current_user.id)
 
-get_db = database.get_db
+@router.get("/{id}", response_model=ticket_schema.TicketOut)
+def get_ticket(id: int, db: Session = Depends(get_db)):
+    return ticket_repository.get_ticket_by_id(db, id)
 
+@router.get("/", response_model=list[ticket_schema.TicketOut])
+def get_all_tickets(db: Session = Depends(get_db)):
+    return ticket_repository.get_all_tickets(db)
 
-@router.get("/", response_model=List[schemas.Showblog])
-def get_all_blogs(db: Session = Depends(get_db)):
-    return blog.get_all(db)
-#only yours blog
-@router.get("/yours", response_model=List[schemas.Showblog])
-def get_your_blogs(
-    db: Session = Depends(get_db),
-    current_user: schemas.User = Depends(oaut2.get_current_user)
-):
-    return blog.get_all_yours(db, current_user)
+@router.get("/my/", response_model=list[ticket_schema.TicketOut])
+def get_my_tickets(db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+    return ticket_repository.get_user_tickets(db, current_user.id)
 
-#just creating blogs
-@router.post("/", status_code = status.HTTP_201_CREATED)
-def create(request:schemas.Blog, db:Session = Depends(get_db),current_user: schemas.User = Depends(oaut2.get_current_user)):
-    return blog.create(db, request, current_user)
+@router.put("/{id}", response_model=ticket_schema.TicketOut)
+def update_ticket(id: int, request: ticket_schema.TicketUpdate, db: Session = Depends(get_db)):
+    return ticket_repository.update_ticket(db, id, request)
 
-@router.delete("/{id}", status_code = status.HTTP_204_NO_CONTENT)
-def delete_blog_by_id(id: int, db:Session = Depends(get_db), current_user: schemas.User = Depends(oaut2.get_current_user)):
-    return blog.delete(id, db, current_user)
-
-@router.put('/{id}', status_code = status.HTTP_202_ACCEPTED)
-def update_blog_by_id(id:int, request:schemas.Blog, db:Session = Depends(get_db), current_user: schemas.User = Depends(oaut2.get_current_user)):
-    return blog.update(id, request, db, current_user)
-
-@router.get("/{id}", status_code=status.HTTP_200_OK, response_model = schemas.Showblog)
-def get_blog_by_id(id:int, db:Session = Depends(get_db), current_user: schemas.User = Depends(oaut2.get_current_user)):
-    return blog.show(id, db, current_user)
+@router.delete("/{id}")
+def delete_ticket(id: int, db: Session = Depends(get_db)):
+    return ticket_repository.delete_ticket(db, id)
