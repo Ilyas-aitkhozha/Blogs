72a1b84bbbccb296f27390fab29eea08bca4825e
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-28 21:04:12 +0500

    added tickets and login routers

--- blog/main.py
+++ blog/main.py
@@ -1,22 +1,15 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import blog, user, login, chat_bot
+from .routers import ticket, user, login, chat_bot
 from contextlib import asynccontextmanager
-
-
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     models.Base.metadata.create_all(bind=engine)
     yield
-
 app = FastAPI(lifespan=lifespan)
-app.include_router(login.router)
-app.include_router(blog.router)
 app.include_router(user.router)
+app.include_router(ticket.router)
+app.include_router(login.router)
 app.include_router(chat_bot.router)
-
-
-
-
