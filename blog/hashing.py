1fbcaa3fe57bd2e0182b1fb62d89489b6f799bc7
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-28 21:05:08 +0500

    from docs checked that i need to use staticmethod to properly use f-ns

--- blog/hashing.py
+++ blog/hashing.py
@@ -2,9 +2,11 @@ from passlib.context import CryptContext
 
 pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-class Hash():
+class Hash:
+    @staticmethod
     def bcrypt(password: str):
         return pwd_cxt.hash(password)
 
-    def verify(hashed_password, plain_password):
+    @staticmethod
+    def verify(hashed_password: str, plain_password: str) -> bool:
         return pwd_cxt.verify(plain_password, hashed_password)
