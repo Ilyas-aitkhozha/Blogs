a835b24b9c3fcd7f123151feda8aa5aa20a216b1
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-28 21:07:19 +0500

    updated some import because of migration of my files

--- blog/routers/login.py
+++ blog/routers/login.py
@@ -1,25 +1,33 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from .. import schemas, models, jwttoken, database
-from ..hashing import Hash
+from blog import models, jwttoken, database
+from blog.schemas.auth import Token
+from blog.hashing import Hash
 
 router = APIRouter(tags=["Login"])
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
-@router.post("/login", response_model=schemas.Token)
+@router.post("/login", response_model=Token)
 def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(database.get_db),
 ):
     user = db.query(models.User).filter(models.User.email == form_data.username).first()
     if not user or not Hash.verify(user.password, form_data.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     token = jwttoken.create_access_token(data={"sub": str(user.id)})
     return {"access_token": token, "token_type": "bearer"}
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)) -> models.User:
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(database.get_db)
+) -> models.User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
