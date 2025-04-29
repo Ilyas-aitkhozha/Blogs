21908084cdb1d9a9a6717f0529be968501116be3
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-29 23:59:18 +0500

    added cors

--- blog/main.py
+++ blog/main.py
@@ -3,12 +3,22 @@ from . import models
 from .database import engine
 from .routers import ticket, user, login, chat_bot
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     models.Base.metadata.create_all(bind=engine)
     yield
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(user.router)
 app.include_router(ticket.router)
 app.include_router(login.router)
