b30ba199fe1631eaf96a63d8ec86c8a3d624d1ae
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-29 17:09:27 +0500

    now admin can put if he is avalaible or not

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from blog.schemas import user as user_schema
 from blog.database import get_db
 from blog.repository import user as user_repository
+from blog.oaut2 import get_current_user
+from blog import models
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -25,3 +27,18 @@ def get_available_users(db: Session = Depends(get_db)):
 @router.get("/{id}", response_model=user_schema.ShowUser)
 def get_user(id: int, db: Session = Depends(get_db)):
     return user_repository.get_user_by_id(db, id)
+
+@router.put("/availability", response_model=user_schema.ShowUser)
+def update_availability(
+    is_available: bool,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(get_current_user)
+):
+    user = db.query(models.User).filter(models.User.id == current_user.id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    user.is_available = is_available
+    db.commit()
+    db.refresh(user)
+    return user
