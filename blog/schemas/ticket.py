09f0400d884a33bf807140566d141aadb70d1565
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-30 02:10:30 +0500

    dfs

--- blog/schemas/ticket.py
+++ blog/schemas/ticket.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
+from pydantic import ConfigDict
 from typing import Optional
 from enum import Enum
 from datetime import datetime
 from blog.schemas import user
-
 
 class TicketStatus(str, Enum):
     open = "open"
@@ -15,7 +15,7 @@ class TicketBase(BaseModel):
     description: str
 
 class TicketCreate(TicketBase):
-    assigned_to_name: Optional[str] = None # for names
+    assigned_to_name: Optional[str] = None
 
 class TicketUpdate(BaseModel):
     status: Optional[TicketStatus] = None
@@ -24,9 +24,8 @@ class TicketUpdate(BaseModel):
 class TicketOut(TicketBase):
     id: int
     status: TicketStatus
-    created_by: user.ShowUser
-    assigned_to: Optional[user.ShowUser]
+    creator: user.ShowUser
+    assignee: Optional[user.ShowUser]
     created_at: datetime
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
