aca074901516100e6f2e3e03217162c649120e63
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-30 12:41:02 +0500

    for auth

--- blog/routers/google_login.py
+++ blog/routers/google_login.py
@@ -17,12 +17,10 @@ oauth.register(
     name='google',
     client_id=os.getenv("GOOGLE_CLIENT_ID"),
     client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-    access_token_url='https://oauth2.googleapis.com/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://www.googleapis.com/oauth2/v1/userinfo',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'}
 )
+
 
 @router.get("/auth/google")
 async def login_via_google(request: Request):
