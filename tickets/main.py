781401e8b87714f61347dcccfd23f72e03f3199e
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-01 01:10:17 +0500

    dimaloh

--- tickets/main.py
+++ tickets/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ticketsystem-5nry.onrender.com","http://localhost:5173/"],
+    allow_origins=["https://ticketsystem-5nry.onrender.com","http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
