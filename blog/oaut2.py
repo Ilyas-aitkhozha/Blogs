f644852c79a88891ed74dde41e434eed9dddaf95
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-04-30 17:37:13 +0500

    now when get_current_user is provided, he is checking if we have cookies (by gmail) or just straight up registerd in website

--- blog/oaut2.py
+++ blog/oaut2.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordBearer
 from . import jwttoken
 from . import models, database
@@ -7,16 +7,21 @@ from sqlalchemy.orm import Session
 
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login", auto_error=False)
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)) -> models.User:
+
+def get_current_user(request: Request, token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)) -> models.User:
+    if not token:
+        token = request.cookies.get("access_token")
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    user_id = jwttoken.verify_token(token, credentials_exception)
+    if not token:
+        raise credentials_exception
 
+    user_id = jwttoken.verify_token(token, credentials_exception)
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if not user:
         raise credentials_exception
