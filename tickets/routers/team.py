f7e0ae5fc4531f29fbaaaf4dad7ea79074c85634
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-01 19:03:21 +0500

    router for team

---
+++ tickets/routers/team.py
@@ -0,0 +1,23 @@
+# routers/team.py
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from tickets.database import get_db
+from tickets import models
+from tickets.schemas.team import TeamCreate, TeamOut, JoinTeam
+from tickets.repository import team as team_repo
+from tickets.oaut2 import get_current_user
+
+router = APIRouter(prefix="/teams", tags=["Teams"])
+
+@router.post("/", response_model=TeamOut, status_code=status.HTTP_201_CREATED)
+def create_team(payload: TeamCreate,
+                db: Session = Depends(get_db),
+                current_user: models.User = Depends(get_current_user)):
+    return team_repo.create_team(db, current_user, payload)
+
+
+@router.post("/join", response_model=TeamOut)
+def join_team(payload: JoinTeam,
+              db: Session = Depends(get_db),
+              current_user: models.User = Depends(get_current_user)):
+    return team_repo.join_team(db, current_user, payload.code)
