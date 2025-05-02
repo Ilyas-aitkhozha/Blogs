ebbf6fe3b0d7609f2fdb3d5a24d787e00a295f39
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-02 15:53:52 +0500

    changed to only cookie response (here exactly just changed the link)

--- tickets/oauth2.py
+++ tickets/oauth2.py
@@ -6,8 +6,7 @@ from sqlalchemy.orm import Session
 
 
 
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login", auto_error=False)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login_in_site", auto_error=False)
 
 
 def get_current_user(request: Request, token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)) -> models.User:
