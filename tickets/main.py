d5fe910f6e5a8b46803849ba9c395b34f68448b4
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-02 18:05:15 +0500

    local or not

--- tickets/main.py
+++ tickets/main.py
@@ -1,6 +1,5 @@
 import os
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.middleware.cors import CORSMiddleware
@@ -8,22 +7,33 @@ from . import models
 from .database import engine
 from tickets.routers.analytics import router as analytics_router
 from .routers import team_ticket, team_user, chat_bot, auth, team
-from .routers.auth import FRONTEND
 
 load_dotenv()
 
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    models.Base.metadata.create_all(bind=engine)
-    yield
-app = FastAPI(lifespan=lifespan)
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
+IS_PRODUCTION = ENVIRONMENT == "production"
+
+# URL-ы
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+FRONTEND_LOCAL_URL = os.getenv("FRONTEND_LOCAL_URL")
+
+origins = []
+if IS_PRODUCTION and FRONTEND_URL:
+    origins.append(FRONTEND_URL)
+elif FRONTEND_LOCAL_URL:
+    origins.append(FRONTEND_LOCAL_URL)
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
 app.add_middleware(
     SessionMiddleware,
     secret_key=os.getenv("SESSION_SECRET_KEY", "super-secret-session")
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("FRONTEND_URL")],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
