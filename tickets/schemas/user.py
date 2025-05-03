13b034c2436916d19f53d8da7af68c1e05ee2c5b
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-04 04:08:57 +0500

    comments

--- tickets/schemas/user.py
+++ tickets/schemas/user.py
@@ -8,17 +8,18 @@ from tickets.schemas.team import TeamOut
 if TYPE_CHECKING:
     from tickets.schemas.ticket import TicketOut
 
-
+#enumki
 class UserRole(str, Enum):
     user = "user"
     admin = "admin"
-
+#base, parent
 class UserBase(BaseModel):
     name: str
-
+#creating with name and password
 class UserCreate(UserBase):
     password: str
 
+#showing user info
 class ShowUser(UserBase):
     id: int
     role: UserRole
@@ -27,7 +28,7 @@ class ShowUser(UserBase):
 
     class Config:
         from_attributes = True
-
+#giving info about users and also ticket that he created
 class UserWithTickets(ShowUser):
     tickets_created: Annotated[List["TicketOut"], Field(default_factory=list)]
     tickets_assigned: Annotated[List["TicketOut"], Field(default_factory=list)]
