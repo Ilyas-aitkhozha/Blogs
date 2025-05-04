25e0ab4cc51b9c6e4122f5a5d400c6a6e1b2d805
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-04 19:10:37 +0500

    In pydantic v1, we used class, config from_attributes = true, now we use V2, with model config

--- tickets/schemas/team.py
+++ tickets/schemas/team.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 from datetime import datetime
+from pydantic import ConfigDict
+
 
 class TeamCreate(BaseModel):
     name: str
@@ -9,9 +11,7 @@ class TeamOut(BaseModel):
     name: str
     code: str
     created_at: datetime
-
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
 
 
 class JoinTeam(BaseModel):
