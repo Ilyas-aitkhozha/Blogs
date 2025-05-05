2f95bcf0273e846b47f8d07067ff521ef7868766
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-05 20:00:40 +0500

    here is the logic for project routers, right now only Create and List projects routers are avalaible

---
+++ tickets/routers/project.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from tickets.database import get_db
+from tickets.schemas.project import ProjectCreate, ProjectOut
+from tickets.repository.project_repository import create_project, get_projects_by_team
+from tickets.routers.dependencies import require_superadmin
+from tickets.oauth2 import get_current_user
+from tickets.models import User, Project
+
+router = APIRouter(prefix="/projects", tags=["Projects"])
+
+@router.post("/", response_model=ProjectOut)
+async def create_new_project(
+    project: ProjectCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(require_superadmin)
+):
+    return create_project(db, project, current_user.id)
+
+@router.get("/", response_model=list[ProjectOut])
+async def list_projects(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    if current_user.role == "superadmin":
+        return db.query(Project).all()
+    return db.query(Project).filter(Project.created_by == current_user.id).all()
