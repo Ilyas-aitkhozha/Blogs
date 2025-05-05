0bb2b1f70afeb1f51faaec0aacf39e393b26027d
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-05 15:24:07 +0500

    Created new schema, didnt want to give id and some other info, when just checking teams

--- tickets/routers/team.py
+++ tickets/routers/team.py
@@ -4,13 +4,13 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from tickets.database import get_db
 from tickets import models
-from tickets.schemas.team import TeamCreate, TeamOut, JoinTeam
+from tickets.schemas.team import TeamCreate, TeamOut, JoinTeam, TeamBriefInfo
 from tickets.repository import team as team_repo
 from tickets.oauth2 import get_current_user
 
 router = APIRouter(prefix="/teams", tags=["Teams"])
 #getting the list of all teams
-@router.get("/check", response_model=List[TeamOut])
+@router.get("/check", response_model=List[TeamBriefInfo])
 def get_teams(db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
     return team_repo.get_user_teams(db, current_user)
 @router.post("/", response_model=TeamOut, status_code=status.HTTP_201_CREATED)
