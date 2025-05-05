32c3573d106e0d14ad0d219de8232fa82098c53a
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-05 15:02:21 +0500

    new router, for listing all the teams u have

--- tickets/routers/team.py
+++ tickets/routers/team.py
@@ -1,4 +1,5 @@
 # routers/team.py
+from typing import List
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from tickets.database import get_db
@@ -8,7 +9,10 @@ from tickets.repository import team as team_repo
 from tickets.oauth2 import get_current_user
 
 router = APIRouter(prefix="/teams", tags=["Teams"])
-
+#getting the list of all teams
+@router.get("/check", response_model=List[TeamOut])
+def get_teams(db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+    return team_repo.get_user_teams(db, current_user)
 @router.post("/", response_model=TeamOut, status_code=status.HTTP_201_CREATED)
 def create_team(payload: TeamCreate,
                 db: Session = Depends(get_db),
