8d1efadcb170a419c47bc421cd3c970768ebaa00
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-10 11:26:52 +0500

    added comments for clarity

--- tickets/repository/project_worker_team.py
+++ tickets/repository/project_worker_team.py
@@ -1,11 +1,9 @@
 from datetime import datetime, timezone
 from sqlalchemy.orm import Session
 from tickets.models import ProjectWorkerTeam, Project, Team, User, UserTeam
-
+#only 1 workers team to one project
 def assign_worker_team(db: Session, project_id: int, team_id: int) -> ProjectWorkerTeam:
-    existing = db.query(ProjectWorkerTeam)\
-                 .filter_by(project_id=project_id)\
-                 .first()
+    existing = db.query(ProjectWorkerTeam).filter_by(project_id=project_id).first()
     if existing:
         raise ValueError("Project already has a worker team")
     link = ProjectWorkerTeam(
