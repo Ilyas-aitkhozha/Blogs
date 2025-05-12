32bf7c189ba7f3a45e2af405fe36b96f515d2d0f
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-12 13:41:59 +0500

    new schemas for glob logic

---
+++ tickets/schemas/worker_team.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict
+
+class WorkerTeamCreate(BaseModel):
+    name: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+class WorkerTeamRead(BaseModel):
+    id: int
+    team_id: int
+    name: str
+    created_at: datetime
+    admin_id: int
+
+    model_config = ConfigDict(from_attributes=True)
