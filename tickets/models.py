e2b782362e6171f2e949c5673e104da8e9fe0f97
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-12 14:03:22 +0500

    added relationship between team and worker_teams

--- tickets/models.py
+++ tickets/models.py
@@ -83,7 +83,7 @@ class User(Base):
     )
 
 
-class Ticket(Base):(Base):
+class Ticket(Base):
     __tablename__ = "tickets"
     id             = Column(Integer, primary_key=True, index=True)
     title          = Column(String, nullable=False)
