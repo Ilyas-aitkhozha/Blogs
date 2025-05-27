23e848c5f70dab7dcb1aea7f18738f00edff6ffe
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-05-27 18:05:02 +0500

    added that if person isnt in the team, he cannot be added to project

--- tickets/repository/project.py
+++ tickets/repository/project.py
@@ -5,10 +5,35 @@ from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 
-from tickets.models import Project, ProjectUser, User
+from tickets.models import Project, ProjectUser, User, UserTeam
 from tickets.schemas.project import ProjectCreate
 from tickets.enums import ProjectRole
+#---------------- helper
+def ensure_user_in_project_team(
+    db: Session,
+    project_id: int,
+    user_id: int
+) -> None:
+    project = db.query(Project).filter(Project.id == project_id).first()
+    if not project:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Project not found"
+        )
 
+    membership = (
+        db.query(UserTeam)
+          .filter(
+            UserTeam.team_id == project.team_id,
+            UserTeam.user_id == user_id
+          )
+          .first()
+    )
+    if not membership:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You must be a member of the team to perform this action"
+        )
 #--------------------------------- CREATE LOGICS
 
 def create_project(
@@ -74,8 +99,10 @@ def add_user_to_project(
     db: Session,
     project_id: int,
     user_id: int,
-    role: ProjectRole
+    role: ProjectRole,
+    current_user_id: int
 ) -> None:
+    ensure_user_in_project_team(db, project_id, current_user_id)
     try:
         link = ProjectUser(
             project_id=project_id,
